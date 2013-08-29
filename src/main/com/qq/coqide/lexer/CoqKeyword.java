e4866e4f597117ee268a1496dcd0bb46668b22cf
Enrique Rodríguez <enriquerodbe@gmail.com>
2013-08-29 20:13:34 -0300

    Abstracted keyword to a class to make syntax highlighter code easier

---
+++ src/main/com/qq/coqide/lexer/CoqKeyword.java
@@ -0,0 +1,10 @@
+package com.qq.coqide.lexer;
+
+import org.jetbrains.annotations.NonNls;
+
+public class CoqKeyword extends CoqElementType {
+
+    public CoqKeyword(@NonNls String debugName) {
+        super(debugName);
+    }
+}
